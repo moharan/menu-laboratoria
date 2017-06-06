8e4061975b0127b5ffd75c48daee8dbd1161f965
RangiBecerra <rangibecerra@gmail.com>
2017-06-06 14:45:49 -0400

    puntos extras

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-//alert("existe");
+function accion() {
+    //alert("funciona");
+   	var elemento = Array.from(document.getElementsByClassName("normal"));
+   	elemento.forEach(function(el){
+	el.setAttribute("class", "normal01");
+  });
+}
+
+function principal() {
+    var elemento= document.getElementById("detalle");
+    elemento.addEventListener("click", accion);
+};
+
+principal();
